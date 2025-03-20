57b300350fcf00bc517781dcc2e687ee81b3d4fc
Sergiy Chyruk <sergii.chyruk@dxc.com>
2025-03-20 15:24:20 +0200

    homework1

---
+++ src/utils/readContacts.js
@@ -0,0 +1,9 @@
+import fs from 'node:fs/promises';
+
+import { PATH_DB } from '../constants/contacts.js';
+
+export const readContacts = async () => {
+  const data = await fs.readFile(PATH_DB, 'utf8');
+
+  return JSON.parse(data);
+};
